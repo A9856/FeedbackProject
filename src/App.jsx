7b572779ffc6ac951795d7d98d42161070af62af
A9856 <adilkhanofficial74@gmail.com>
2025-06-14 12:05:51 +0530

    Added new fila and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // import ProfilePage from './pages/ProfilePage'
 import FormPage from './pages/FormPage'
+import './assests/css/main.css'
 
 export default function App() {
     return (
